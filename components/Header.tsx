1357c161e71dde0428e11cfdddb0aa2b96aacb92
Önder <ondersabahat@gmail.com>
2024-11-15 15:14:02 +0300

    first commit

---
+++ components/Header.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import Image from "next/image";
+import { Button } from "./ui/button";
+import Search from "./Search";
+import FileUploader from "./FileUploader";
+import { signOutUser } from "@/lib/actions/user.actions";
+
+const Header = ({
+  userId,
+  accountId,
+}: {
+  userId: string;
+  accountId: string;
+}) => {
+  return (
+    <header className="header">
+      <Search />
+      <div className="header-wrapper">
+        <FileUploader ownerId={userId} accountId={accountId} />
+        <form
+          action={async () => {
+            "use server";
+
+            await signOutUser();
+          }}
+        >
+          <Button className="sign-out-button" type="submit">
+            <Image
+              src="/assets/icons/logout.svg"
+              alt="logout"
+              width={24}
+              height={24}
+              className="w-6"
+            />
+          </Button>
+        </form>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
